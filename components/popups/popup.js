5dc00d68e5e817f529df3f55a8c37216ec792f9e
Marotik <kitoram3@gmail.com>
2023-01-08 20:58:03 +0900

    Popup 관련 plugin에서 Popup Component 변수명을 일치시켜줌.

--- components/popups/popup.js
+++ components/popups/popup.js
@@ -1,13 +1,13 @@
-import Alert from './PopAlert';
-import Confirm from './PopConfirm';
-import UserRegistCompleted from './PopUserRegistCompleted'; // 회원가입 완료 팝업
-import Terms from './PopTerms'; // 약관 안내 팝업
-import LeaveReason from './PopLeaveReason'; // 회원 탈퇴 사유입력
+import PopAlert from './PopAlert'; // 안내 및 경고 팝업
+import PopConfirm from './PopConfirm'; // 확인 팝업
+import PopUserRegistCompleted from './PopUserRegistCompleted'; // 회원가입 완료 팝업
+import PopTerms from './PopTerms'; // 약관 안내 팝업
+import PopLeaveReason from './PopLeaveReason'; // 회원 탈퇴 사유입력
 
 
 export default {
   showAlertPopup: (message, closeCallback = () => {}) => {
-    new Alert({
+    new PopAlert({
       propsData: {
         title: message,
         okCallback: ({ $destroy }) => $destroy(),
@@ -15,9 +15,9 @@ export default {
       },
     }).$mount();
   },
-  Alert,
-  Confirm,
-  UserRegistCompleted,
-  Terms,
-  LeaveReason,
+  PopAlert,
+  PopConfirm,
+  PopUserRegistCompleted,
+  PopTerms,
+  PopLeaveReason,
 };
