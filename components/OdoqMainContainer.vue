194614a38117d9567c68d05b8f48b27457605dbc
Marotik <kitoram3@gmail.com>
2023-01-16 02:06:05 +0900

    로그인 여부 판별 변수를 추가한 MainContainer

--- components/OdoqMainContainer.vue
+++ components/OdoqMainContainer.vue
@@ -3,17 +3,24 @@
     <OdoqQuestionContainer></OdoqQuestionContainer>
     <OdoqWeekendTimerContainer></OdoqWeekendTimerContainer>
     <div class="verticalLine2"></div>
+    <div>오오오 로그인했너</div>
     <OdoqAnswerContainer></OdoqAnswerContainer>
   </div>
 </template>
 
 <script>
+import {mapGetters} from "vuex";
 export default {
     data () {
         return {
             weekday: 1,
             onOffSeason: true,
         }
+    },
+    computed: {
+      ...mapGetters({
+        isLogin: 'user/userAuthStore/isLogin'
+      })
     }
 }
 </script>
