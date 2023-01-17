c2927577d6f8df1a96afb8ca444f816b6937062a
Marotik <kitoram3@gmail.com>
2023-01-18 01:43:32 +0900

    사용자 인증 믹스인 로직 정리

--- mixins/UserAuthMixin.js
+++ mixins/UserAuthMixin.js
@@ -13,10 +13,16 @@ export default {
     userGrade: null,
     isNormalUser: null,
   }),
-  beforeMount() {
-    if (!Utils.getCookie(document.cookie, 'jwt')) return;
-    console.log('UserAuthMixin beforeMount called ', document.cookie);
-
-    this.$store.commit('user/userAuthStore/checkLogin', {isLogin: true, cookie: document.cookie});
-  }
+  /**
+   * request에서 JWT를 확인하여 로그인 상태를 확인한다.
+   * 회원가입후 자동로그인의 경우, req.headers를 얻을 수 없다.
+   */
+  asyncData({ store, req }){
+    try {
+      const cookie = req? req.headers.cookie : document.cookie;
+      if (!Utils.getCookie(cookie, 'jwt')) return;
+      store.commit('user/userAuthStore/checkLogin', { isLogin: true, cookie });
+    } catch (TypeError) {
+    }
+  },
 }
