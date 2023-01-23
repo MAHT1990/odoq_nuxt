87fe010862bf99c9842c9533f4a9a9382176e87f
Marotik <kitoram3@gmail.com>
2023-01-24 03:45:16 +0900

    question 관련 getter를 추가하여, questionStore에서 관련 state를 반영하여 serving 하도록 수정.

--- components/OdoqQuestionContainer.vue
+++ components/OdoqQuestionContainer.vue
@@ -1,30 +1,29 @@
 <template>
-    <div class="question_container">
-        <div class="question_container_header">
-            <p class="season_box">Season {{season}} #{{code}}</p>
-            <p class="countdown_box"></p>
-        </div>
-        <div class="question_box">
-            <img  v-if="hasImg" src="@/assets/img/testQuestion.jpg" alt="load Error">
-            <img v-else src="@/assets/img/testQuestion2.jpg" alt="load Error">
-        </div>
+  <div class="question_container">
+    <div class="question_container_header">
+      <p class="season_box">Season {{ question.season }} #{{ question.code }}</p>
+      <p class="countdown_box"></p>
     </div>
+    <div class="question_box">
+      <img v-if="hasImg" :src="question.img_url" alt="load Error">
+      <img v-else src="@/assets/img/testQuestion2.jpg" alt="load Error">
+    </div>
+  </div>
 </template>
 
 <script>
+import {mapGetters} from "vuex";
+
 export default {
-    data() {
-        return {
-            hasImg: true,
-            season: 1,
-            code: 222,
-        }
-    },
-    props: {
-      questionImage: {
-        type: String,
-        default: '',
-      },
+  data() {
+    return {
+      hasImg: true,
     }
+  },
+  computed: {
+    ...mapGetters({
+      question: 'question/questionStore/question',
+    })
+  },
 }
 </script>
