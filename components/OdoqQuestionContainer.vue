d64243717194f8ec0a55fef701065a203fa7795e
Marotik <kitoram3@gmail.com>
2023-01-25 01:36:07 +0900

    timer를 추가하였음

--- components/OdoqQuestionContainer.vue
+++ components/OdoqQuestionContainer.vue
@@ -2,7 +2,7 @@
   <div class="question_container">
     <div class="question_container_header">
       <p class="season_box">Season {{ question.season }} #{{ question.code }}</p>
-      <p class="countdown_box"></p>
+      <p class="countdown_box">{{ timerFormat }}</p>
     </div>
     <div class="question_box">
       <img v-if="hasImg" :src="question.img_url" alt="load Error">
@@ -12,18 +12,58 @@
 </template>
 
 <script>
-import {mapGetters} from "vuex";
-
 export default {
+  props: {
+    question: {
+      type: Object,
+      default: null,
+    }
+  },
   data() {
     return {
       hasImg: true,
+      secondRemain: null,
+      intervalId: null,
     }
   },
   computed: {
-    ...mapGetters({
-      question: 'question/questionStore/question',
-    })
+    timerFormat() {
+      const hour = parseInt(this.secondRemain / 3600);
+      const min = parseInt((this.secondRemain % 3600) / 60);
+      const sec = parseInt(this.secondRemain % 60);
+
+      return `${this.timerFormatter(hour)}:${this.timerFormatter(min)}:${this.timerFormatter(sec)}`;
+    }
+  },
+  watch: {
+    secondRemain(timeRemain) {
+      if (timeRemain <= 0) {
+        clearInterval(this.intervalId)
+        this.$emit('nextQuestionLoadEvent')
+      }
+    },
+    question(oldQuestion) {
+      setTimeout(this.timerStarter, 1000);
+    }
+  },
+  created() {
+    this.timerStarter();
+  },
+  methods: {
+    timerFormatter(num) {
+      return num < 10 ? "0" + num : num;
+    },
+    timerStarter() {
+      if (this.question.second_remain > 0) {
+        this.secondRemain = this.question.second_remain;
+        // Timer 시작.
+        this.intervalId = setInterval(() => {
+          this.secondRemain--
+        }, 1000)
+      } else {
+        this.secondRemain = 'THIS IS THE LAST';
+      }
+    }
   },
 }
 </script>
