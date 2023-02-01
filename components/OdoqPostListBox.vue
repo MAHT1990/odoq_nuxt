81130a3d2d82be634ece9d444938c9287969465b
Marotik <kitoram3@gmail.com>
2023-02-02 01:14:33 +0900

    댓글 박스 추가.

---
+++ components/OdoqPostListBox.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="comment_list_box">
+    <OdoqPostLine
+      v-for="post in arrayPost"
+      :key="post.id"
+      :post="post"
+    />
+  </div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex';
+export default {
+  computed: {
+    ...mapGetters({
+      arrayPost: 'post/postStore/arrayPost',
+    })
+  },
+  watch: {
+    'arrayPost': function (val) {
+      console.log(val);
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
