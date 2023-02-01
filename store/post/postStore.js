cf0b151590e9377c9c05667df77c006369963e59
Marotik <kitoram3@gmail.com>
2023-02-02 01:15:10 +0900

    댓글 관련 Store 수정 보완.

--- store/post/postStore.js
+++ store/post/postStore.js
@@ -1,22 +1,34 @@
 const state = () => ({
-  post: {},
+  arrayPost: [],
 });
 
 const actions = {
-  getPost({ commit }) {
+  async getPost({ commit }) {
     console.log('getPost gogogo');
-    // const res = await this.$axios.get('post');
-    // commit('setPost', res.data);
+    const res = await this.$axios.get('post/');
+    if (res.data.result === 'success') {
+      commit('setArrayPost', res.data.data.posts);
+      // console.dir(res.data.data.posts, {
+      //   showHidden: true,
+      //   colors: true,
+      //   depth: 4
+      // });
+    }
+  },
+  async createPost({ commit }, postData) {
+    console.log('createPost path is here');
+    await this.$axios.post('post/', postData)
   }
 };
 
 const mutations = {
-  setPost(state, axiosResData) {
+  setArrayPost(state, axiosPostData) {
+    state.arrayPost = axiosPostData;
   }
 };
 
 const getters = {
-  post: (state) => (state.comment),
+  arrayPost: (state) => (state.arrayPost),
 };
 
 export default {
