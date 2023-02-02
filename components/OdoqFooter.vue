9f9545ed7574df99e1dfdcdf76592744710af12d
Marotik <kitoram3@gmail.com>
2023-02-03 01:36:54 +0900

    Create OdoqFooter.vue

---
+++ components/OdoqFooter.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="footer_container">
+    <div class="verticalLine3"></div>
+    <footer>
+      <div class="aboutUs_container">
+        <div class="aboutUs_header">
+          About us.
+        </div>
+        <div class="aboutUs_content">
+          SDIJ 수학강사 김정문 <br>
+          <a href="https://jmmath9.com/" target="_blank" rel="noopener noreferrer">jmmath9.com</a>
+        </div>
+      </div>
+      <div class="caution_container">
+        <div class="caution_header">
+          Caution.
+        </div>
+        <div class="caution_content">
+          저작권에 보호를 받는 컨텐츠입니다. 출처표시 없이 사용하는 경우
+          법적인 책임을 질 수 있습니다.
+          (지적재산권 최대 5년의 징역/5천만 원의 벌금형에 처할 수 있습니다.)
+          <br>좋은 취지와 의지를 가지고 하는 일인 만큼 저작권 침해에 관해서는
+          엄중하게 대응할 예정입니다.
+          본 컨텐츠가 무단으로 사용되고있는 것을 목격하신 분들은
+          <a href="mailto:odoq.help@gmail.com">odoq.help@gmail.com</a>으로 꼭 연락 부탁드립니다.
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OdoqFooter"
+}
+</script>
+
+<style scoped>
+
+</style>
