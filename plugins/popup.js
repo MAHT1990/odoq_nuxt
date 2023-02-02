d794a076c71ee72591fafb134b303713d424fc46
Marotik <kitoram3@gmail.com>
2023-02-03 01:39:40 +0900

    Popup 구조 변경

--- plugins/popup.js
+++ plugins/popup.js
@@ -1,4 +1,26 @@
 import Vue from 'vue';
-import Popup from '@/components/popups/popup';
+import PopAlert from '@/components/popups/PopAlert'; // 안내 및 경고 팝업
+import PopConfirm from '@/components/popups/PopConfirm'; // 확인 팝업
+import PopUserRegistCompleted from '@/components/popups/PopUserRegistCompleted'; // 회원가입 완료 팝업
+import PopTerms from '@/components/popups/PopTerms'; // 약관 안내 팝업
+import PopLeaveReason from '@/components/popups/PopLeaveReason'; // 회원 탈퇴 사유입력
 
+
+const Popup = {
+  showAlertPopup: (message, closeCallback = () => {}) => {
+    new PopAlert({
+      propsData: {
+        title: message,
+        okCallback: ({ $destroy }) => $destroy(),
+        closeCallback,
+      },
+    }).$mount();
+  },
+  PopAlert,
+  PopConfirm,
+  PopUserRegistCompleted,
+  PopTerms,
+  PopLeaveReason,
+};
+export default Popup
 Vue.prototype.$popup = Popup;
