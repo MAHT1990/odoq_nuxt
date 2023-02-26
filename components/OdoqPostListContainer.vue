1f6cb8fb6dd044db7b4e6c0492b04feb45b644f0
Marotik <kitoram3@gmail.com>
2023-02-27 00:41:32 +0900

    pagination 버튼 추가
    .

--- components/OdoqPostListContainer.vue
+++ components/OdoqPostListContainer.vue
@@ -17,9 +17,15 @@
       />
     </div>
     <div class="comment_list_navigator">
-      <button id="comment_list_navigator_prev"><i class="fa-solid fa-angle-left"></i>&nbsp;이전</button>
-      <div id="comment_list_navigator_page_numbering">1/전체페이지수</div>
-      <button id="comment_list_navigator_next">다음&nbsp;<i class="fa-solid fa-angle-right"></i></button>
+      <button
+        id="comment_list_navigator_prev"
+        @click="prevPage"
+      ><i class="fa-solid fa-angle-left"></i>&nbsp;이전</button>
+      <div id="comment_list_navigator_page_numbering">{{currentPage}}/{{totalPages}}</div>
+      <button
+        id="comment_list_navigator_next"
+        @click="nextPage"
+      >다음&nbsp;<i class="fa-solid fa-angle-right"></i></button>
     </div>
   </div>
 </template>
@@ -30,12 +36,27 @@ export default {
   computed: {
     ...mapGetters({
       arrayPost: 'post/postStore/arrayPost',
+      totalPosts: 'post/postStore/totalPosts',
+      currentPage: 'post/postStore/currentPage',
+      totalPages: 'post/postStore/totalPages',
     })
   },
   methods: {
     emptyClick() {
       console.log('emptyClick');
     },
+    prevPage() {
+      this.$store.dispatch('post/postStore/getPost', {
+        pageNumber: this.currentPage - 1,
+        pageSize: 7,
+      });
+    },
+    nextPage() {
+      this.$store.dispatch('post/postStore/getPost', {
+        pageNumber: this.currentPage + 1,
+        pageSize: 7,
+      });
+    }
   }
 }
 </script>
