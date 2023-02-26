d96b40443076c7f5c40184f5e5616ffe995828b2
Marotik <kitoram3@gmail.com>
2023-02-27 00:42:44 +0900

    pagination
    pagination에서 총 페이지, 현재페이지, 총 댓글수

--- store/post/postStore.js
+++ store/post/postStore.js
@@ -1,13 +1,22 @@
 const state = () => ({
   arrayPost: [],
+  totalPosts: 0,
+  currentPage: 0,
+  totalPages: 0,
 });
 
 const actions = {
   async getPost({ commit }, objAboutPage) {
     console.log('getPost gogogo');
-    const res = await this.$axios.get('post/', objAboutPage);
+    const res = await this.$axios.get('post/', {
+      params: objAboutPage,
+    });
     if (res.data.result === 'success') {
       commit('setArrayPost', res.data.data.posts);
+      commit('setTotalPosts', res.data.data.total_posts);
+      commit('setCurrentPage', res.data.data.current_page);
+      commit('setTotalPages', res.data.data.total_pages);
+      console.log('postStore의 getPost의 결과: ', res.data.data);
       // console.dir(res.data.data.posts, {
       //   showHidden: true,
       //   colors: true,
@@ -19,8 +28,11 @@ const actions = {
     console.log('createPost path is here');
     const res = await this.$axios.post('post/', postData);
     if (res.data.result === 'success') {
-      console.log(res.data.data.posts);
+      console.log(res.data.data);
       commit('setArrayPost', res.data.data.posts);
+      commit('setTotalPosts', res.data.data.total_posts);
+      commit('setCurrentPage', res.data.data.current_page);
+      commit('setTotalPages', res.data.data.total_pages);
     }
     return 1;
   }
@@ -29,11 +41,23 @@ const actions = {
 const mutations = {
   setArrayPost(state, axiosPostData) {
     state.arrayPost = axiosPostData;
+  },
+  setTotalPosts(state, axiosTotalPosts) {
+    state.totalPosts = axiosTotalPosts;
+  },
+  setCurrentPage(state, axiosCurrentPage) {
+    state.currentPage = axiosCurrentPage;
+  },
+  setTotalPages(state, axiosTotalPages) {
+    state.totalPages = axiosTotalPages;
   }
 };
 
 const getters = {
   arrayPost: (state) => (state.arrayPost),
+  totalPosts: (state) => (state.totalPosts),
+  currentPage: (state) => (state.currentPage),
+  totalPages: (state) => (state.totalPages),
 };
 
 export default {
