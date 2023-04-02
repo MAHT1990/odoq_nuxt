01e20c8c99056d7694e15dd487800dc15fb0ba57
Marotik <kitoram3@gmail.com>
2023-04-03 00:24:45 +0900

    주말 answer container v-show 수정
    .

--- components/OdoqMainContainer.vue
+++ components/OdoqMainContainer.vue
@@ -12,7 +12,7 @@
     <div class="verticalLine2"></div>
     <div>{{ weekday }}</div>
     <odoq-answer-container
-      v-if="isLogin && !isWeekend"
+      v-show="isLogin && !isWeekend"
       :question="question"
       :user-info="userInfo"
     />
