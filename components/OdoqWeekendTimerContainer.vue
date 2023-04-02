9ad198d77c0d95b84c5865712fb52eb5379e9909
Marotik <kitoram3@gmail.com>
2023-04-02 18:48:29 +0900

    주말 시간 재계산
    .

--- components/OdoqWeekendTimerContainer.vue
+++ components/OdoqWeekendTimerContainer.vue
@@ -53,7 +53,9 @@ export default {
       // 한국은 월요일(1)이 첫 요일이다.
       // 주말은 6, 0이다.
       const now = moment();
-      const nextWeek = moment().add(7 - (now.day()-1)%7, 'days').hour(0).minute(0).second(0);
+      console.log('## OdoqWeekendTimerContainer\'s calcRemainTime > now is ', now);
+      const nextWeek = moment().add(7 - (now.day() + 6)%7, 'days').hour(0).minute(0).second(0);
+      console.log('## OdoqWeekendTimerContainer\'s calcRemainTime > nextWeek is ', nextWeek);
       const remainTime = nextWeek.diff(now);
       return parseInt(remainTime / 1000);
     }
