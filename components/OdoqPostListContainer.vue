0c77f47393a10bb24f0ee0c0b8ed4a9b82d2b849
Marotik <kitoram3@gmail.com>
2023-05-17 01:35:21 +0900

    flag 변환시 watcher 아직 필요없음
    .

--- components/OdoqPostListContainer.vue
+++ components/OdoqPostListContainer.vue
@@ -87,22 +87,22 @@ export default {
      * 1. filteringFlag가 변경되면, 해당 filteringFlag에 맞게 post를 나열.
      * @return {Promise<void>}
      */
-    async filteringFlag() {
-      await this.$store.dispatch('post/postStore/getPosts', {
-        pageNumber: 1,
-        pageSize: 7,
-        filteringFlag: this.filteringFlag,
-        userId: this.userInfo.userId,
-      });
-    },
+    // async filteringFlag() {
+    //   await this.$store.dispatch('post/postStore/getPosts', {
+    //     pageNumber: 1,
+    //     pageSize: 7,
+    //     filteringFlag: this.filteringFlag,
+    //     userId: this.userInfo.userId,
+    //   });
+    // },
     /**
      * @description
      * 1. orderingFlag가 변경되면, 해당 orderingFlag에 맞는 색상으로 버튼 색상을 변경한다.
      */
-    orderingFlag() {
-      this.$refs.buttonOrderAll.style.color = this.orderingFlag === 'latest' ? this.buttonColor.activated : this.buttonColor.deactivated;
-      this.$refs.buttonOrderMy.style.color = this.orderingFlag === 'likeCount' ? this.buttonColor.activated : this.buttonColor.deactivated;
-    },
+    // orderingFlag() {
+    //   this.$refs.buttonOrderAll.style.color = this.orderingFlag === 'latest' ? this.buttonColor.activated : this.buttonColor.deactivated;
+    //   this.$refs.buttonOrderMy.style.color = this.orderingFlag === 'likeCount' ? this.buttonColor.activated : this.buttonColor.deactivated;
+    // },
   },
   methods: {
     /**
