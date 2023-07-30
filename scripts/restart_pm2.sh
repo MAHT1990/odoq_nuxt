918343f016c1976f2763360ab7466cf96a640083
Marotik <kitoram3@gmail.com>
2023-07-31 01:41:42 +0900

    cron update
    .

--- scripts/restart_pm2.sh
+++ scripts/restart_pm2.sh
@@ -2,7 +2,7 @@
 LOG_FILE="/home/ubuntu/.pm2/logs/pm2_restart.log"
 
 echo "Restarting PM2 at $(date)" >> $LOG_FILE
-pm2 restart all >> $LOG_FILE
-pm2 save >> $LOG_FILE
+/home/ubuntu/.nvm/versions/node/v16.16.0/bin/pm2 restart all >> $LOG_FILE
+/home/ubuntu/.nvm/versions/node/v16.16.0/bin/pm2 save >> $LOG_FILE
 echo "PM2 restarted" >> $LOG_FILE
 
